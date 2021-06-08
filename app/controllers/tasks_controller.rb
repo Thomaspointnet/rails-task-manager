0f08c6337ac4031c5eb1342a0c831777a04a9cfe
Thomas Petitprez <t.petitprez@outlook.fr>
2021-06-08 15:55:43 +0200

    CRUD OK

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,38 @@ class TasksController < ApplicationController
     id = params[:id]
     @task = Task.find(id)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    redirect_to task_path(task) if task.save
+  end
+
+  def edit
+    id = params[:id]
+    @task = Task.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @task = Task.find(id)
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    id = params[:id]
+    @task = Task.find(id)
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
